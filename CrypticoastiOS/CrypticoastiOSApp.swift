5cf2203ea150857068db2ca0f93df362b6449634
Felix Boudnik <felix@boudnik.de>
2023-05-17 09:43:36 +0200

    Initialize Project

---
+++ CrypticoastiOS/CrypticoastiOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  CrypticoastiOSApp.swift
+//  CrypticoastiOS
+//
+//  Created by Felix Boudnik on 17.05.23.
+//
+
+import SwiftUI
+
+@main
+struct CrypticoastiOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
