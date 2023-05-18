d89a80dd288a9583df8b2d1a717eb78f599d4687
Felix Boudnik <felix@boudnik.de>
2023-05-18 16:13:15 +0200

    Add Data Service Class

--- CrypticoastiOS/Core/Home/ViewModel/HomeViewModel.swift
+++ CrypticoastiOS/Core/Home/ViewModel/HomeViewModel.swift
@@ -6,6 +6,7 @@
 //
 
 import Foundation
+import Combine
 
 class HomeViewModel: ObservableObject {
     
@@ -13,10 +14,18 @@ class HomeViewModel: ObservableObject {
     @Published var allCoins: [Coin] = []
     @Published var portfolioCoins: [Coin] = []
     
+    private let dataService = CoinDataService()
+    private var cancellables = Set<AnyCancellable>()
+    
     init() {
-        DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) {
-            self.allCoins.append(DeveloperPreview.instance.coin)
-            self.portfolioCoins.append(DeveloperPreview.instance.coin)
-        }
+        addSubscribers()
+    }
+    
+    func addSubscribers() {
+        dataService.$allCoins
+            .sink { [weak self] (returnedCoins) in
+                self?.allCoins = returnedCoins
+            }
+            .store(in: &cancellables)
     }
 }
